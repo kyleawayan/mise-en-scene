99ccff170e9a7e19015b1a86c4ec539a27ab67a3
Kyle Awayan <56655579+kyleawayan@users.noreply.github.com>
2021-05-08 18:18:45 -0700

    make create room command

--- MakeJSONs.h
+++ MakeJSONs.h
@@ -4,6 +4,8 @@
 
 #include <cpprest/json.h>
 
+
+
 utility::stringstream_t createJoinJson(std::wstring roomId, std::wstring username)
 {
 	utility::stringstream_t stream;
@@ -13,6 +15,22 @@ utility::stringstream_t createJoinJson(std::wstring roomId, std::wstring usernam
 	web::json::value payloadObject = web::json::value::object();
 	payloadObject[U("roomId")] = web::json::value::string(roomId);
 	payloadObject[U("username")] = web::json::value::string(username);
+
+	joinObject[U("action")] = action;
+	joinObject[U("payload")] = payloadObject;
+
+	joinObject.serialize(stream);
+	return stream;
+}
+
+utility::stringstream_t createRoomJson(std::wstring roomId)
+{
+	utility::stringstream_t stream;
+	web::json::value joinObject = web::json::value::object();
+	web::json::value action = web::json::value::string(U("create"));
+
+	web::json::value payloadObject = web::json::value::object();
+	payloadObject[U("roomId")] = web::json::value::string(roomId);
 
 	joinObject[U("action")] = action;
 	joinObject[U("payload")] = payloadObject;
--- mise-en-scene.cpp
+++ mise-en-scene.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <codecvt>
+#include <string>
 // vcpkg install cpprestsdk[default-features,websockets]:x64-windows
 #include <cpprest/ws_client.h>
 #include "Keyboard.h"
@@ -8,9 +10,25 @@ using namespace std;
 using namespace web;
 using namespace web::websockets::client;
 
+random_device rd;
+mt19937 rng(rd());
+
+int get_random_int(int min, int max)
+{
+    uniform_int_distribution<int> uni(min, max);
+    return uni(rng);
+}
+
 wstring convertStringToWString(string string)
 {
     return wstring(string.begin(), string.end());
+}
+
+// https://stackoverflow.com/questions/4358870/convert-wstring-to-string-encoded-in-utf-8/12903901
+string wstring_to_utf8(const std::wstring& str)
+{
+    wstring_convert<codecvt_utf8<wchar_t>> myconv;
+    return myconv.to_bytes(str);
 }
 
 int main()
@@ -27,10 +45,27 @@ int main()
     websocket_client client;
     client.connect(wsUrl).then([]() { cout << "Conntected to Mise-en-Scene server" << endl; }).wait();
 
-    cout << "Enter room ID: ";
-    string roomIdString;
-    getline(cin, roomIdString);
-    roomId = convertStringToWString(roomIdString);
+    cout << "Would you like to create a room? (y/n) ";
+    string createRoom;
+    getline(cin, createRoom);
+
+    if (createRoom == "y")
+    {
+        // to do: make these functions
+        roomId = to_wstring(get_random_int(1000, 9999));
+        utility::stringstream_t createJson = createRoomJson(roomId);
+        wstring createWString = createJson.str();
+
+        websocket_outgoing_message sendToSocketJoinJson;
+        sendToSocketJoinJson.set_utf8_message(wstring_to_utf8(createWString));
+        client.send(sendToSocketJoinJson).then([]() { cout << "Creating room..." << endl; }).wait();
+    }
+    else {
+        cout << "Enter room ID: ";
+        string roomIdString;
+        getline(cin, roomIdString);
+        roomId = convertStringToWString(roomIdString);
+    }
 
     cout << "Enter a username: ";
     string usernameString;
@@ -38,12 +73,13 @@ int main()
     username = convertStringToWString(usernameString);
 
     utility::stringstream_t joinJson = createJoinJson(roomId, username);
+    wstring joinWString = joinJson.str();
 
-    // cout << str(joinJson) << endl;
+    websocket_outgoing_message sendToSocketJoinJson;
+    sendToSocketJoinJson.set_utf8_message(wstring_to_utf8(joinWString));
+    client.send(sendToSocketJoinJson).then([]() { cout << "Joining room..." << endl; }).wait();
 
-    // websocket_outgoing_message msg;
-    // msg.set_utf8_message(joinJson);
-    // client.send(joinJsoin).then([]() { /* Successfully sent the message. */ }).wait();
+
 
     // hitKey(0xB3);
 }
